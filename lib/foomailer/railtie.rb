8a5ceb0fd75a6ff2452c99722d46a21863dee37b
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-02-26 16:51:02 +1100

    Logging

--- lib/foomailer/railtie.rb
+++ lib/foomailer/railtie.rb
@@ -3,8 +3,10 @@ require 'foomailer/mailer'
 module FooMailer
   class Railtie < ::Rails::Railtie
     initializer "foomailer.add_delivery_method" do
+      Rails.logger.warn("foomailer.add_delivery_method initializer called")
       ActiveSupport.on_load(:action_mailer) do
         add_delivery_method :foo, FooMailer::Mailer
+        Rails.logger.warn("ActionMailer :foo delivery method added")
         ActiveSupport.run_load_hooks(:foo_mailer, FooMailer::Mailer)
       end
     end
